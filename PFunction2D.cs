99a760153facf7ae4b5e6e9cad7959df530c6e6b
Stefano Angeleri <weltall2@gmail.com>
2012-07-25 20:50:25 +0200

    Added licenses references.

--- PFunction2D.cs
+++ PFunction2D.cs
@@ -1,11 +1,38 @@
-﻿using System;
+﻿/* Copyright (C) 2012 Fairmat SRL (info@fairmat.com, http://www.fairmat.com/)
+ * Author(s): Stefano Angeleri (stefano.angeleri@fairmat.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DVPLDOM;
 
 namespace PFunction2D
 {
-    public class PFunction2D
+    public class PFunction2D : PFunction
     {
+        public PFunction2D(Project project)
+            : base(project)
+        {
+        }
+
+        public PFunction2D(Project project, string variableName)
+            : base(project, variableName)
+        {
+        }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,21 @@
-﻿using System.Reflection;
+﻿/* Copyright (C) 2012 Fairmat SRL (info@fairmat.com, http://www.fairmat.com/)
+ * Author(s): Stefano Angeleri (stefano.angeleri@fairmat.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Mono.Addins;
@@ -6,23 +23,23 @@ using Mono.Addins;
 // The following lines tell that the assembly is an addin
 [assembly: Addin("PFunction2D", "1.0.16", Category = "Functions")]
 [assembly: AddinDependency("Fairmat", "1.0")]
-[assembly: AddinAuthor("")]
+[assembly: AddinAuthor("Fairmat SRL")]
 [assembly: AddinDescription("A 2D implementation of Function.")]
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PFunction2D")]
 [assembly: AssemblyDescription("A 2D implementation of Function.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Fairmat SRL")]
 [assembly: AssemblyProduct("PFunction2D")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Fairmat SRL 2012")]
+[assembly: AssemblyTrademark("Fairmat")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -32,11 +49,11 @@ using Mono.Addins;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
