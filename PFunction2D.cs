a57a734b60a0707848ffcc4b2f7c95145d34c4bf
Stefano Angeleri <weltall2@gmail.com>
2012-07-25 18:53:10 +0200

    Added the first skeleton of the plug-in.

---
+++ PFunction2D.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PFunction2D
+{
+    public class PFunction2D
+    {
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Mono.Addins;
+
+// The following lines tell that the assembly is an addin
+[assembly: Addin("PFunction2D", "1.0.16", Category = "Functions")]
+[assembly: AddinDependency("Fairmat", "1.0")]
+[assembly: AddinAuthor("")]
+[assembly: AddinDescription("A 2D implementation of Function.")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PFunction2D")]
+[assembly: AssemblyDescription("A 2D implementation of Function.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PFunction2D")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8178efb4-cd71-47a5-8f32-d6b3fca75b18")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
