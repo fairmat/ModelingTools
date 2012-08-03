97efeb0cf66010a5a78d6b0534a5f5f24457bdf9
matteot <matteo.tesser@gmail.com>
2012-08-03 18:53:56 +0300

    Update Properties/AssemblyInfo.cs
    edited description

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -21,16 +21,16 @@ using System.Runtime.InteropServices;
 using Mono.Addins;
 
 // The following lines tell that the assembly is an addin.
-[assembly: Addin("Point Function 2D", "1.0.0", Category = "Functions")]
+[assembly: Addin("Point Function 2D", "1.0.0", Category = "Modeling Tools")]
 [assembly: AddinDependency("Fairmat", "1.0")]
 [assembly: AddinAuthor("Fairmat SRL")]
-[assembly: AddinDescription("A 2D function defined by value interpolation.")]
+[assembly: AddinDescription("A 2D function defined by interpolating or fitting data.")]
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Point Function 2D")]
-[assembly: AssemblyDescription("A 2D function defined by value interpolation.")]
+[assembly: AssemblyDescription("A 2D function defined by interpolating or fitting data.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fairmat SRL")]
 [assembly: AssemblyProduct("Point Function 2D")]
