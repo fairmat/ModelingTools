9c945b6f00fafd833ff1b4ffa11401a2b90a6606
Stefano Angeleri <weltall2@gmail.com>
2012-08-12 12:04:05 +0200

    Added NUnit based tests to ModelingTools.

---
+++ ModelingTools.Tests/PFunction2D/TestPFunction2DInterpolation.cs
@@ -0,0 +1,119 @@
+/* Copyright (C) 2012 Fairmat SRL (info@fairmat.com, http://www.fairmat.com/)
+ * Author(s): Stefano Angeleri (stefano.angeleri@fairmat.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DVPLDOM;
+using DVPLI;
+using DVPLUtils;
+using Mono.Addins;
+using NUnit.Framework;
+using DVPLSolver;
+
+namespace PFunction2D
+{
+    /// <summary>
+    /// Tests PFunction2D Interpolations.
+    /// </summary>
+    [TestFixture]
+    public class TestPFunction2DInterpolation
+    {
+        /// <summary>
+        /// Initializes the backend to run the tests.
+        /// </summary>
+        [SetUp]
+        public void Init()
+        {
+            DVPLI.PluginsManager.Init();
+            Mono.Addins.AddinManager.Registry.ResetConfiguration();
+            Mono.Addins.AddinManager.Registry.Update(new Mono.Addins.ConsoleProgressStatus(6));
+			DVPLI.Engine.Parser.NewContext();
+        }
+
+        /// <summary>
+        /// Setups some sample data:
+        ///   x  0 100
+        ///   0  0  50
+        /// 100 50 100
+        /// in a PFunction2D.
+        /// </summary>
+        /// <returns>A PFunction2D populated with the sample data</returns>
+        private PFunction2D SetTestData1()
+        {
+            // Prepare some simple data.
+            Vector coordinatesX = new Vector(2);
+            Vector coordinatesY = new Vector(2);
+            Matrix values = new Matrix(2);
+
+            coordinatesX[0] = 0.0;
+            coordinatesX[1] = 100.0;
+
+            coordinatesY[0] = 0.0;
+            coordinatesY[1] = 100.0;
+
+            values[0, 0] = 0.0;
+            values[0, 1] = 50.0;
+            values[1, 1] = 100.0;
+            values[1, 0] = 50.0;
+
+            return new PFunction2D(coordinatesX, coordinatesY, values);
+        }
+
+        /// <summary>
+        /// Tries the linear interpolation at point 50, 50
+        /// with the test data 1. In that point the result should be 50.
+        /// </summary>
+        [Test]
+        public void TestLinearInterpolation1()
+        {
+            PFunction2D func = SetTestData1();
+            func.Interpolation = EInterpolationType.LINEAR;
+            func.Parse(null);
+            Assert.AreEqual(50, func.Evaluate(50, 50));
+		}
+
+        /// <summary>
+        /// Tries the Zero Order interpolation at point 50, 50
+        /// with the test data 1. In that point the result should be 100
+        /// as it should take the point on the bottom right, which is valued as 100.
+        /// </summary>
+        [Test]
+        public void TestZeroOrderInterpolation1()
+        {
+            PFunction2D func = SetTestData1();
+            func.Interpolation = EInterpolationType.ZERO_ORDER;
+            func.Parse(null);
+            Assert.AreEqual(100, func.Evaluate(50, 50));
+        }
+
+        /// <summary>
+        /// Tries the Left Zero Order interpolation at point 50, 50
+        /// with the test data 1. In that point the result should be 0
+        /// as it should take the point on the upper left, which is valued as 0.
+        /// </summary>
+        [Test]
+        public void TestZeroOrderLeftInterpolation1()
+        {
+            PFunction2D func = SetTestData1();
+            func.Interpolation = EInterpolationType.ZERO_ORDER_LEFT;
+            func.Parse(null);
+            Assert.AreEqual(0, func.Evaluate(50, 50));
+        }
+	}
+}
+
--- ModelingTools.Tests/Properties/AssemblyInfo.cs
+++ ModelingTools.Tests/Properties/AssemblyInfo.cs
@@ -16,47 +16,34 @@
  */
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Mono.Addins;
 
-// The following lines tell that the assembly is an addin.
-[assembly: Addin("Point Function 2D", "1.0.3", Category = "Modeling Tools")]
-[assembly: AddinDependency("Fairmat", "1.0")]
-[assembly: AddinAuthor("Fairmat SRL")]
-[assembly: AddinDescription("A 2D function defined by interpolating or fitting data.")]
-
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Point Function 2D")]
-[assembly: AssemblyDescription("A 2D function defined by interpolating or fitting data.")]
+[assembly: AssemblyTitle("ModelingTools.Tests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fairmat SRL")]
-[assembly: AssemblyProduct("Point Function 2D")]
-[assembly: AssemblyCopyright("Copyright © Fairmat SRL 2012")]
+[assembly: AssemblyProduct("ModelingTools.Tests")]
+[assembly: AssemblyCopyright("Copyright © Fairmat 2012")]
 [assembly: AssemblyTrademark("Fairmat")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8178efb4-cd71-47a5-8f32-d6b3fca75b18")]
+[assembly: Guid("41245ca5-c40c-40fc-9e89-06b28ab4944c")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
-[assembly: InternalsVisibleTo("ModelingTools.Tests")]
-
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
