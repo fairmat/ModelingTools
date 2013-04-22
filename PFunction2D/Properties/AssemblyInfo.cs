6682a864cb8478facfc2dc1d7df81b7c0628798b
Francesco Biondi <francesco.biondi@fairmat.com>
2013-04-22 17:37:45 +0200

    Bumped version numbers

--- PFunction2D/Properties/AssemblyInfo.cs
+++ PFunction2D/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 using Mono.Addins;
 
 // The following lines tell that the assembly is an addin.
-[assembly: Addin("Point Function 2D", "1.0.3", Category = "Modeling Tools")]
+[assembly: Addin("Point Function 2D", "1.0.4", Category = "Modeling Tools")]
 [assembly: AddinDependency("Fairmat", "1.0")]
 [assembly: AddinAuthor("Fairmat SRL")]
 [assembly: AddinDescription("A 2D function defined by interpolating or fitting data.")]
