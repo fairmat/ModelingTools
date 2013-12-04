7473c573952bbcefe3aaa9160c829555d810a8ac
Francesco Biondi <francesco.biondi@fairmat.com>
2013-12-04 10:11:36 +0100

    Bumped version number of DatesGenerator to 1.0.19

--- DatesGenerator/Properties/AssemblyInfo.cs
+++ DatesGenerator/Properties/AssemblyInfo.cs
@@ -52,10 +52,10 @@ using Mono.Addins;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.18")]
-[assembly: AssemblyFileVersion("1.0.18")]
+[assembly: AssemblyVersion("1.0.19")]
+[assembly: AssemblyFileVersion("1.0.19")]
 
 // Plugin information
-[assembly: Addin("Dates Generator", "1.0.18", Category = "Modeling Tools")]
+[assembly: Addin("Dates Generator", "1.0.19", Category = "Modeling Tools")]
 [assembly: AddinDependency("Fairmat", "1.0")]
 [assembly: AddinAuthor("Fairmat SRL")]
