f6454aa5dcd3893e6b643da601bd31b46e1e2c4a
Giulio Fornasaro <giulio.fornasaro@fairmat.com>
2024-04-19 16:38:55 +0200

    [RD-FFK-NET8] Re-added assembly with addin informations

---
+++ DatesGenerator/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using Mono.Addins;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Plugin information
+[assembly: Addin("Dates Generator", "1.0.29", Category = "Modeling Tools")]
+[assembly: AddinDependency("Fairmat", "1.0")]
+[assembly: AddinAuthor("Fairmat SRL")]
