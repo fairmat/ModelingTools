3bcb4ea3df165a9ad8003a9ee32263e94b513553
Mattia Trevisani <mattia.trevisani@fairmat.com>
2024-05-24 12:14:55 +0200

    [RD-FFK-NET8] Fixed addin names for gui projects. fairmat/dev/fairmatserver#3486

--- PFunction2DGui/Properties/AssemblyInfo.cs
+++ PFunction2DGui/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using Mono.Addins;
 
-[assembly: Addin("Point Function 2D", "1.0.5", Category = "Modeling Tools")]
+[assembly: Addin("Point Function 2D Gui", "1.0.5", Category = "Modeling Tools")]
 [assembly: AddinDependency("Fairmat", "1.0")]
 [assembly: AddinAuthor("Fairmat SRL")]
-[assembly: AddinDescription("A 2D function defined by interpolating or fitting data.")]
+[assembly: AddinDescription("Gui plugin for Point Function 2D plugin.")]
